fe64cc32ca1dbbe2a008b9b2252928ecd60068d0
Kola132 <kolmakovn111@gmail.com>
2023-02-01 23:26:21 +0300

    upd

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@
 
 // import 'package:clean/firebase_options.dart';
 import 'package:dio/dio.dart';
-import 'package:firebase_core/firebase_core.dart';
+// import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
@@ -10,9 +10,9 @@ import 'model/api.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-      // options: DefaultFirebaseOptions.currentPlatform,
-      );
+  // await Firebase.initializeApp(
+  //     // options: DefaultFirebaseOptions.currentPlatform,
+  //     );
   runApp(const MyApp());
 }
 
